154dd8a338abc8f6e3a69a9a970aaa8b83e4986f
ohm <98101484+ohmreborn@users.noreply.github.com>
2024-03-22 11:00:14 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,120 +1,189 @@
-#include "star.h"
+#include <iostream>
 #include <fstream>
-using namespace std;
+#include <cblas.h>
 
-double abs(double* a);
-void minu(double* a ,double* b,double res[2]);
-void plu(double* a ,double* b,double res[2]);
-void mul(double a,double b[2],double res[2]);
-void slove(Star *system,int n_star,double time,double dt,string filename);
+void count_star(int *n_star,std::string filename){
+    *n_star = 0;
 
+    std::fstream newfile;
+    newfile.open(filename,std::ios::in);
+    std::string line;
 
-
-int main(){
-    double m1 = 1;
-    double m2 = 333000;
-    double m3 = 1000;
-    double r1[2] = {1,0};
-    double v1[2] = {0,sqrt(m2)};
-    double r2[2] = {0,0};
-    double v2[2] = {0,0};
-    double r3[2] = {-0.5,0};
-    double v3[2] = {0,-100};
-    double time = 1;
-    double dt = 5e-5;//time/200;
-
-    Star earth = Star(m1,r1,v1,"earth");
-    Star sun = Star(m2,r2,v2,"sun");
-    Star earth2 = Star(m3,r3,v3,"earth2");
-    Star system[] = {earth,sun,earth2};
-    const int n_system = sizeof(system)/sizeof(system[0]);
-    slove(system,n_system,time,dt,"results.csv");
-
-    return 0;
-}
-
-double abs(double* a){
-    double total = 0;
-    for (int i=0;i<2;i++){
-        total = total + a[i]*a[i];
-    }
-    return sqrt(total);
-}
-
-void minu(double* a ,double* b,double res[2]){
-    for (int i=0;i<2;i++){
-        res[i] = a[i] - b[i];
-    }
-}
-
-void plu(double* a ,double* b,double res[2]){
-    for (int i=0;i<2;i++){
-        res[i] = a[i] + b[i];
-    }
-}
-
-void mul(double a,double b[2],double res[2]){
-    for (int i=0;i<2;i++){
-        res[i] = b[i]*a;
-    }
-}
-
-void slove(Star *system,int n_star,double time,double dt,string filename){
-    ofstream myfile;
-    myfile.open (filename);
-    myfile << "x,y,star,frame" << endl;
-
-    double k = 1;
-    double distance;
-    double a[n_star][2];
-    double acclelation[2];
-    double r[n_star][2];
-    double dv[2];
-    double dr[2];
-    double t = 0;
-    int frame = 0;
-
-
-
-    while (time > t){
-        for (int i=0;i<n_star;i++){
-            myfile << system[i].r[0] << "," << system[i].r[1] << "," << system[i].name << "," << frame << endl;
-            for (int j=0;j<2;j++){
-                a[i][j] = 0;
+    if(newfile.is_open()){
+    while(getline(newfile, line)){
+        if (line != "mass,x,y,vx,vy,name"){  
+            (*n_star)++;
             }
         }
+        newfile.close();
+    }else{
+        std::cout << "error" << std::endl;;
+        }
 
-        // conmpute a acclelation;
-        for (int i=0;i<n_star;i++){
-            for (int j=0;j<n_star;j++){
-                if (i != j){
-                    minu(system[j].r,system[i].r,r[i]);
-                    distance = abs(r[i]);
+}
 
-                    mul(k*system[j].m/pow(distance,3),r[i],acclelation);
-                    
-                    for (int l=0;l<2;l++){
-                        a[i][l] = a[i][l] + acclelation[l];
+void read(std::string filename,int dim,double **v,double **r,double *m,std::string *star_name){
+    const char delimeter = ',';
+    std::fstream newfile(filename);
+    std::string line;
+    int length;
+    int line_ind = 0;
+    int ind;
+    std::string element;
+    
+    if(newfile.is_open()){
+        while(getline(newfile, line)){
+            if (line != "mass,x,y,vx,vy,name"){  
+                length = line.length();
+                for (int i=0;i<length;i++){
+                    if(line[i] != delimeter){
+                        element +=  line[i];
                     }
+                    if((line[i] == delimeter) || (i==length-1)){
+                        ind = i + 1;
+                        m[line_ind] = stod(element);
+                        element = "";
+                        break;
+                    }
+                }
+                for (int j=0;j<dim;j++){
+                    for (int i=ind;i<length;i++){
+                        if(line[i] != delimeter){
+                            element +=  line[i];
+                        }
+                        if((line[i] == delimeter) || (i==length-1)){
+                            ind = i + 1;
+                            r[line_ind][j] = stod(element);
+                            element = "";
+                            break;
+                        }
+                    }
+                }
+                for (int j=0;j<dim;j++){
+                    for (int i=ind;i<length;i++){
+                        if(line[i] != delimeter){
+                            element +=  line[i];
+                        }
+                        if((line[i] == delimeter) || (i==length-1)){
+                            ind = i + 1;
+                            v[line_ind][j] = stod(element);
+                            element = "";
+                            break;
+                        }
+                    }
+                }
+                for (int i=ind;i<length;i++){
+                    if(line[i] != delimeter){
+                        element +=  line[i];
+                    }
+                    if((line[i] == delimeter) || (i==length-1)){
+                        star_name[line_ind] = element;
+                        element = "";
+                        break;
+                    }
+                }
+                line_ind ++;
+            }
+        }
+        newfile.close();
+        }else{
+        std::cout << "error";
+    }
+
+}
+
+void slove(std::string output,int dim,int n_star,double **v,double **r,double *m,std::string* star_name){
+    const double G = 1;
+    double time = 1;
+    double dt = 2e-5;
+    int step = time/dt;
+
+    double distance;
+    double *norm_distance = new double[dim];
+    double *dr = new double[dim];
+
+    double **dv = new double*[n_star];
+
+    for (int i=0;i<n_star;i++){
+        dv[i] = new double[dim];
+    }
+
+    std::ofstream myfile;
+    myfile.open (output);
+    myfile << "x,y,star,frame" << std::endl;
+
+    for (int i=0;i<step;i++){
+        for (int ai=0;ai<n_star;ai++){
+            for (int aj=0;aj<dim;aj++){
+                dv[ai][aj] = 0;
+            }
+        for (int d=0;d<dim;d++){
+            myfile << r[ai][0] << "," << r[ai][1] << ",";
+        }
+        myfile << star_name[ai] << "," << i << std::endl;
+        }
+        for (int star1=0;star1<n_star;star1++){
+            for (int star2=0;star2<n_star;star2++){
+                if (star1 != star2){
+                    cblas_dcopy(dim,r[star2],1,norm_distance,1);
+                    cblas_daxpy(dim,-1,r[star1],1,norm_distance,1);
+                    distance = cblas_dnrm2(dim,norm_distance,1);
+                    cblas_dscal(dim,(G * m[star2]/(pow(distance,3))),norm_distance,1);
+                    cblas_daxpy(dim,1,norm_distance,1,dv[star1],1);
                 }
             }
         }
-    
-        for (int i=0;i<n_star;i++){
-            // update a velocity
-
-            mul(dt,a[i],dv);
-            plu(system[i].v,dv,system[i].v);
-            
-            // update position
-            mul(dt,system[i].v,dr);
-            plu(system[i].r,dr,system[i].r);
+        for (int star=0;star<n_star;star++){
+            cblas_dscal(dim,dt,dv[star],1);
+            cblas_daxpy(dim,1,dv[star],1,v[star],1);
+            cblas_dcopy(dim,v[star],1,dr,1);
+            cblas_dscal(dim,dt,dr,1);
+            cblas_daxpy(dim,1,dr,1,r[star],1);
+            // std::cout << r[star][0] << ", " << r[star][1];
+            // std::cout << std::endl;
         }
 
-        frame = frame + 1;
-        t = t + dt;
     }
-
+    myfile.close();
+    for (int i=0;i<n_star;i++){
+        delete[] dv[i];
+    }
+    delete[] norm_distance;
+    delete[] dv;
+    delete[] dr;
 }
 
-// g++ main.cpp -o main && ./main && python3 animation.py
+int main(){
+    int n_star;
+    const int dim = 2;
+    const char delimeter = ',';
+    const std::string filename = "../input.csv";
+    const std::string output = "../output.csv";
+
+    count_star(&n_star,filename);
+    
+    std::cout << n_star << std::endl;
+    std::string *star_name = new std::string[n_star];
+    double **v = new double*[n_star];
+    double **r = new double*[n_star];
+    double *m = new double[n_star];
+    for (int i=0;i<n_star;i++){
+        v[i] = new double[dim];
+        r[i] = new double[dim];
+    }
+    std::cout << "read" << std::endl;
+    read(filename,dim,v,r,m,star_name);
+    std::cout << "read sucess" << std::endl;
+
+    slove(output,dim,n_star,v,r,m, star_name);
+
+    for (int i=0;i<dim;i++){
+        delete[] v[i];
+        delete[] r[i];
+    }
+    delete[] v;
+    delete[] r;
+    delete[] m;
+
+    return 0;
+}
